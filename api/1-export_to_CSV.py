3afc1c9ffd65662048bd64cac1e5d73050467ef8
Abiodun Kumuyi <a.kumuyi@alustudent.com>
2023-11-05 12:08:10 -0800

    Export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -12,41 +12,41 @@ Args:
     analyze completed tasks and export to CSV.
 """
 
+
 if __name__ == "__main__":
     import csv
     import json
     import sys
     import urllib.request
+
     # Get the employee's ID from the sys module and format it into the URL
     # https://jsonplaceholder.typicode.com/users/{employee_id}
     employee_id = sys.argv[1]
     url1 = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
     url2 = f"https://jsonplaceholder.typicode.com/users/{employee_id}/"
+
     # Create the request objects.
     request_object1 = urllib.request.Request(url1)
     request_object2 = urllib.request.Request(url2)
+
     # Get data from the server.
     with urllib.request.urlopen(request_object1) as get_data1:
         response_data1 = json.load(get_data1)
     with urllib.request.urlopen(request_object2) as get_data2:
         response_data2 = json.load(get_data2)
-    # Use a list comprehension to filter completed tasks
+
+    # Use a list comprehension to filter completed tasks.
     completed_tasks = [task for task in response_data1 if task['completed']]
+
     # Get Employee Name
     employee_name = response_data2["name"]
-    # Print output in the requested format Employee EMPLOYEE_NAME is
-    # done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS)
-    # Create a CSV file and write the data to it
-    with open(f"{employee_id}.csv", 'w', newline='') as csvfile:
-        fieldnames = ["USER_ID", "USERNAME",
-                      "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()  # Write the header row
-        # Write data for each completed task
+
+    # Create a CSV file and write the data to it.
+    with open(f"{employee_id}.csv", "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+
+        # Write data for each completed task.
         for task in completed_tasks:
-            writer.writerow({
-                "USER_ID": employee_id,
-                "USERNAME": employee_name,
-                "TASK_COMPLETED_STATUS": task['completed'],
-                "TASK_TITLE": task['title']
-            })
+            writer.writerow(
+                [employee_id, employee_name, task["completed"], task["title"]]
+            )
