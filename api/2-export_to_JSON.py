08d7153bb6565c3c8a95d683044a41e9a7870000
Abiodun Kumuyi <a.kumuyi@alustudent.com>
2023-11-05 14:42:20 -0800

    Export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,81 +1,76 @@
 #!/usr/bin/python3
 """
-This script fetches and analyzes completed tasks for a
-given employee from a remote API and exports the data in JSON format.
-
-It takes an employee's ID as a command-line argument,
-retrieves the tasks associated with that employee,
-displays the number of completed tasks, and
-exports the data to a JSON file in the specified format.
+This script fetches and analyzes completed tasks for a given
+employee from a remote API and exports the data in JSON format.
 
 Usage:
     $ python script_name.py employee_id
 
 Args:
-    employee_id (int): The ID of the employee for
-    whom you want to analyze completed tasks and export to JSON.
+    employee_id (int): The ID of the employee for whom you want
+    to analyze completed tasks and export the data in JSON format.
+
+Example:
+    $ python script_name.py 1
 """
 
-
-def export_to_json(employee_id, tasks):
-    """Exports the task data to JSON format.
-
-    Args:
-        employee_id (int): The ID of the employee
-        for whom the tasks are owned by.
-        tasks (list): A list of tasks.
-
-    Returns:
-        str: A JSON string containing the task data.
-    """
-
-    json_data = {
-        employee_id: [
-            {
-                "task": task["title"],
-                "completed": task["completed"],
-                "username": task.get("username", None),
-            }
-            for task in tasks
-        ]
-    }
-
-    return json.dumps(json_data, indent=4)
-
-
 if __name__ == "__main__":
-    import csv
     import json
     import sys
     import urllib.request
 
-    # Get the employee's ID from the sys module and format it into the URL
-    # https://jsonplaceholder.typicode.com/users/{employees_id}
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py employee_id")
+        sys.exit(1)
+
+    # Get the employee's ID from the command line argument
     employee_id = sys.argv[1]
-    url1 = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-    url2 = f"https://jsonplaceholder.typicode.com/users/{employee_id}/"
 
-    # Create the request objects.
-    request_object1 = urllib.request.Request(url1)
-    request_object2 = urllib.request.Request(url2)
+    # Define the URLs for employee tasks and user information
+    tasks_url = (
+            "https://jsonplaceholder.typicode.com/todos"
+            "(userId={employee_id})"
+    )
 
-    # Get data from the server.
-    with urllib.request.urlopen(request_object1) as get_data1:
-        response_data1 = json.load(get_data1)
-    with urllib.request.urlopen(request_object2) as get_data2:
-        response_data2 = json.load(get_data2)
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
 
-    # Use a list comprehension to filter completed tasks.
-    completed_tasks = [
-        task for task in response_data1 if task["completed"]
-    ]
+    # Create the request objects for tasks and user information
+    tasks_request = urllib.request.Request(tasks_url)
+    user_request = urllib.request.Request(user_url)
 
-    # Get Employee Name
-    employee_name = response_data2["username"]
+    try:
+        # Get data from the server for tasks and user information
+        with (
+                urllib.request.urlopen(tasks_request) as tasks_response,
+                urllib.request.urlopen(user_request) as user_response,
+        ):
+            tasks_data = json.load(tasks_response)
+            user_data = json.load(user_response)
+    except urllib.error.URLError as e:
+        print(f"Failed to fetch data: {e}")
+        sys.exit(1)
 
-    # Export the task data to JSON format.
-    json_data = export_to_json(employee_id, completed_tasks)
+    # Extract the username
+    username = user_data.get("username", "")
 
-    # Write the JSON data to a file.
-    with open(f"{employee_id}.json", "w") as outfile:
-        outfile.write(json_data)
+    # Prepare the data in the specified JSON format
+    task_list = (
+            [
+                {
+                    "task": task["title"],
+                    "completed": task["completed"],
+                    "username": username,
+                }
+                for task in tasks_data
+            ]
+    )
+
+    # Create a dictionary with the user_id as the key
+    data_to_export = {employee_id: task_list}
+
+    # Write the data to a JSON file with the filename USER_ID.json
+    json_filename = f"{employee_id}.json"
+    with open(json_filename, "w") as json_file:
+        json.dump(data_to_export, json_file, indent=4)
+
+    print(f"Data exported to {json_filename}")
